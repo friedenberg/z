d27cb62dad3069c14f14df8f1913bf6ff3b8390b
Sasha Friedenberg <181492+friedenberg@users.noreply.github.com>
2021-09-24 21:55:11 -0700

    add ulimit to files_guard

--- util/files_guard/main.go
+++ util/files_guard/main.go
@@ -3,6 +3,9 @@ package files_guard
 import (
 	"io"
 	"os"
+	"os/exec"
+	"strconv"
+	"strings"
 )
 
 type openFilesGuard struct {
@@ -12,9 +15,24 @@ type openFilesGuard struct {
 var openFilesGuardInstance *openFilesGuard
 
 func init() {
+	limitCmd := exec.Command("ulimit", "-S", "-n")
+	output, err := limitCmd.Output()
+
+	if err != nil {
+		panic(err)
+	}
+
+	limitStr := strings.TrimSpace(string(output))
+
+	limit, err := strconv.ParseInt(string(limitStr), 10, 64)
+
+	if err != nil {
+		panic(err)
+	}
+
 	openFilesGuardInstance = &openFilesGuard{
-		//TODO read from OS and determine dynamically
-		channel: make(chan struct{}, 240),
+		//TODO subtract from unknowable
+		channel: make(chan struct{}, limit),
 	}
 }
 
@@ -22,8 +40,20 @@ func (g *openFilesGuard) Lock() {
 	g.channel <- struct{}{}
 }
 
+func (g *openFilesGuard) LockN(n int) {
+	for i := 0; i < n; i++ {
+		g.channel <- struct{}{}
+	}
+}
+
 func (g *openFilesGuard) Unlock() {
 	<-g.channel
+}
+
+func (g *openFilesGuard) UnlockN(n int) {
+	for i := 0; i < n; i++ {
+		<-g.channel
+	}
 }
 
 func Create(s string) (f *os.File, err error) {
@@ -50,4 +80,11 @@ func Open(s string) (f *os.File, err error) {
 func Close(f io.Closer) error {
 	defer openFilesGuardInstance.Unlock()
 	return f.Close()
+}
+
+func CombinedOutput(c *exec.Cmd) ([]byte, error) {
+	openFilesGuardInstance.LockN(3)
+	defer openFilesGuardInstance.UnlockN(3)
+
+	return c.CombinedOutput()
 }
