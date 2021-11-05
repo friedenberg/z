0083481aba33315265f2c36377c12cb6b46a4a81
Sasha Friedenberg <181492+friedenberg@users.noreply.github.com>
2021-11-04 19:07:09 -0700

    move kasten interface to own file

---
+++ lib/kasten.go
@@ -0,0 +1,13 @@
+package lib
+
+import "github.com/friedenberg/z/lib/zettel"
+
+type Kasten interface {
+	Init(Umwelt, map[string]interface{}) (err error)
+	BasePath() string
+	GetAll() (zettels []string, err error)
+	GetNormalizedPath(a string) (b string, err error)
+	NewId() (id zettel.Id, err error)
+	Hydrate(z *Zettel, includeBody bool) (err error)
+	CommitTransaction(Umwelt) error
+}
--- lib/zettel.go
+++ lib/zettel.go
@@ -1,20 +1,8 @@
 package lib
 
 import (
-	"github.com/friedenberg/z/lib/zettel"
 	"github.com/friedenberg/z/lib/zettel/metadata"
 )
-
-//TODO-P3 rename to LocalStore
-type Kasten interface {
-	Init(Umwelt, map[string]interface{}) (err error)
-	BasePath() string
-	GetAll() (zettels []string, err error)
-	GetNormalizedPath(a string) (b string, err error)
-	NewId() (id zettel.Id, err error)
-	Hydrate(z *Zettel, includeBody bool) (err error)
-	CommitTransaction(Umwelt) error
-}
 
 type Zettel struct {
 	Umwelt
